bea988cc00ba0bb917fcd7d477199ac7f617290d
GiftdesignRO <emagsmartbigdata@gmail.com>
2026-05-26 15:44:30 +0300

    Fix SMTP transport

--- server.js
+++ server.js
@@ -164,11 +164,18 @@ const api = axios.create({
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT || 465),
+  port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
