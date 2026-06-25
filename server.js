e3c23bca1b4240e3cf142c011f6c87a0c594620d
GiftdesignRO <emagsmartbigdata@gmail.com>
2026-06-25 13:07:05 +0300

    Test route

--- server.js
+++ server.js
@@ -1377,7 +1377,12 @@ app.put(
 app.get(
   '/admin/merchantpro/orders-test',
     async (req, res) => {
-    try {
+
+  return res.json({
+    test: "BALAUR 2026"
+  });
+
+  try {{
       const response = await api.get('/api/v2/orders/93283017');
 
       res.json(response.data);
