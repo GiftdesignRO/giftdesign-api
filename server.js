7ce133f4125786951f252e9f8264d8cb9fc27a6f
GiftdesignRO <emagsmartbigdata@gmail.com>
2026-07-02 16:31:36 +0300

    Send MerchantPro order status fields as strings

--- server.js
+++ server.js
@@ -1285,9 +1285,9 @@ const orderNumber =
          ? 24.9
          : 0;
       const merchantPayload = {
-        payment_status: ['awaiting'],
-        payment_method_code: [paymentMethodCode],
-        shipping_status: ['awaiting'],
+        payment_status: 'awaiting',
+        payment_method_code: paymentMethodCode,
+        shipping_status: 'awaiting',
         shipping_method_id: delivery_method === 'Curier rapid' ? 3 : 0,
         shipping_amount: shippingAmount,
 
